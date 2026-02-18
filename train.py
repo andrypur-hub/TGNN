88ee4e2cc71896f6f3855ab297316ed1058e3776
andrypur-hub <andry.pur@gmail.com>
2026-02-18 15:40:26 +0700

    Update train.py

--- train.py
+++ train.py
@@ -20,7 +20,8 @@ from model.node_classifier import NodeClassifier
 DATA_PATH = "/content/drive/MyDrive/ProjectPython/TGNN/Dataset/elliptic_bitcoin_dataset"
 
 print("Loading Elliptic dataset...")
-events_by_time, n_nodes = load_elliptic_events(DATA_PATH)
+
+events_by_time, labels_by_time, n_nodes = load_elliptic_events(DATA_PATH)
 
 DIM = 64
 tgnn = TGNN(DIM)
