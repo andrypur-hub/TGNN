d47f1e30d174de5af7bb6c9291ce4c8fcc0183f1
andrypur-hub <andry.pur@gmail.com>
2026-02-18 15:21:03 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
-import sys
-import os
+import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 
 import torch
 import torch.nn.functional as F
@@ -9,39 +7,61 @@ import torch.nn.functional as F
 from data.loader.elliptic_loader import load_elliptic_events
 from model.memory import NodeMemory
 from model.tgnn import TGNN
+from model.node_classifier import NodeClassifier
+
+DATA_PATH = "/content/drive/MyDrive/Datasets/elliptic_bitcoin_dataset"
 
 print("Loading Elliptic dataset...")
-
-events, n_nodes = load_elliptic_events("/content/drive/MyDrive/ProjectPython/TGNN/Dataset/elliptic_bitcoin_dataset")
+events_by_time, n_nodes = load_elliptic_events(DATA_PATH)
 
 DIM = 64
-model = TGNN(DIM)
+tgnn = TGNN(DIM)
 memory = NodeMemory(n_nodes, DIM)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+classifier = NodeClassifier(DIM)
 
-EPOCHS = 2
+optimizer = torch.optim.Adam(
+    list(tgnn.parameters()) + list(classifier.parameters()),
+    lr=1e-3
+)
+
+EPOCHS = 3
 
 for epoch in range(EPOCHS):
 
     total_loss = 0
 
-    for e in events:
+    for t, events in events_by_time.items():
 
-        hu = memory.get([e.src])
-        hv = memory.get([e.dst])
-        x = torch.tensor([[e.x[0]]], dtype=torch.float)
+        # 1️⃣ ingest transactions
+        for e in events:
+            hu = memory.get([e.src])
+            hv = memory.get([e.dst])
+            x = torch.tensor([[e.x[0]]], dtype=torch.float)
 
-        hu_new, hv_new, score = model(hu, hv, x)
+            hu_new, hv_new, _ = tgnn(hu, hv, x)
+            memory.update([e.src], hu_new)
+            memory.update([e.dst], hv_new)
 
-        y = torch.tensor([[e.y]], dtype=torch.float)
-        loss = F.binary_cross_entropy_with_logits(score, y)
+        # 2️⃣ node prediction
+        nodes = []
+        labels = []
+
+        for e in events:
+            nodes.append(e.src)
+            labels.append(e.y)
+
+        nodes = list(set(nodes))
+
+        h = memory.get(nodes)
+        logits = classifier(h).squeeze()
+
+        y = torch.tensor(labels[:len(logits)], dtype=torch.float)
+
+        loss = F.binary_cross_entropy_with_logits(logits, y)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-        memory.update([e.src], hu_new)
-        memory.update([e.dst], hv_new)
 
         total_loss += loss.item()
 
