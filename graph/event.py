7e69ff92dee5c43f6afcb5c84d4a464a3aa7c5b6
andrypur-hub <andry.pur@gmail.com>
2026-02-18 14:13:40 +0700

    Update event.py

--- graph/event.py
+++ graph/event.py
@@ -1,1 +1,21 @@
+class GraphEvent:
+    """
+    Represent one temporal interaction in the graph.
+
+    src : source node id
+    dst : destination node id
+    t   : timestamp
+    x   : feature vector (list or tensor)
+    y   : label (0 normal, 1 fraud)
+    """
+
+    def __init__(self, src, dst, t, x, y):
+        self.src = src
+        self.dst = dst
+        self.t = t
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"GraphEvent(src={self.src}, dst={self.dst}, t={self.t}, y={self.y})"
 
